fbe7477b8fc17a667868a1e14af602e1d98ebe8a
Edu <eduardokaue277@gmail.com>
2024-04-30 00:12:43 -0300

    Feat: Atualizaço~es de Banco e Comandos CRUD

---
+++ Models/Onibus.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BUSAPI.Models.Enuns;
+
+namespace BUSAPI.Models
+{
+    public class Onibus
+    {
+        public int Id { get; set; }
+
+        public string Linha { get; set; }
+
+        public string NomeLinha { get; set; }
+
+        public string PontoPartida { get; set; }
+
+        public string PontoFinal { get; set; }
+
+        public int TempoEspera { get; set; }
+
+        public Situacoes Situacao { get; set; }
+
+        public bool Acessibilidade { get; set; }
+    }
+}
